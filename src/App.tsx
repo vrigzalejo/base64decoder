9d731672c6751a9af9ce3c4530b59e98f6620c57
Brigido Alejo <brigsalejo@gmail.com>
2020-09-27 01:22:53 +0800

    added material-ui and clear button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Image from "react-image-enlarger";
+import Button from "@material-ui/core/Button";
 import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
   const [zoomed, setZoomed] = useState(false);
+  const textAreaRef: any = useRef<HTMLTextAreaElement>(null);
 
   return (
     <div className="App">
       <h2>Decode Base64 to Image</h2>
-      <textarea value={input} onInput={(e: React.ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value)} ></textarea>
-      {input && <div>
-        <h2>Result</h2>
-        <Image
-          style={{ width: "30vw", height: "auto" }}
-          zoomed={zoomed}
-          src={`data:image/png;base64,${input}`}
-          onClick={() => setZoomed(true)}
-          onRequestClose={() => setZoomed(false)}
-        />
-      </div>}
+      <textarea
+        ref={textAreaRef}
+        value={input}
+        onInput={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+          setInput(e.target.value)
+        }
+      ></textarea>
+      {input && (
+        <div>
+          <Button 
+            variant="contained"
+            size="large"
+            color="primary" 
+            onClick={() => {
+              setInput("");
+              if (textAreaRef.current !== null) {
+                textAreaRef.current.focus();
+              }
+            }
+          }>
+            Clear
+          </Button>
+          <h2>Result</h2>
+          <Image
+            style={{ width: "30vw", height: "auto" }}
+            zoomed={zoomed}
+            src={`data:image/png;base64,${input}`}
+            onClick={() => setZoomed(true)}
+            onRequestClose={() => setZoomed(false)}
+          />
+        </div>
+      )}
     </div>
   );
 }
